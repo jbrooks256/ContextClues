3193c44b55ad493a6670e83771c9d4445869d97d
Justin Brooks <justinbrooks@Justins-MacBook-Pro.local>
2020-06-03 16:20:00 -0500

    last commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var weapons = ['paper clip', 'gun', 'knife', 'spoon', 'fork', 'spork', 'bat', 's
 
 for (let i = 1; i <= 100; i++) {
     var h3 = document.createElement('h3');
-    h3.innerText = 'Accusation ' + i;
+    h3.innerText = `Accusation ${i}`;
     document.body.appendChild(h3);
     h3.addEventListener('click', function () {
         alert(`Accusation  ${i}: I accuse ${friends[i%friends.length]}, with the ${weapons[i%weapons.length]} in the ${locations[i%locations.length]}!`);
